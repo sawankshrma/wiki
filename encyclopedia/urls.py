1800b430aaf77275fdf048e1f1db24604ca1ecdf
sawankshrma <thesawan47@gmail.com>
2025-06-14 21:37:55 +0530

    urls uptade

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,7 +5,9 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.entry, name="entry"),
+    path("wiki/", views.index, name="index1"),
     path("search/", views.search_redirect, name="search"),  # For form submission
+
     path("newpage/", views.add, name="add"),
     path("change/<str:title>", views.change, name="change"),
     path("random/", views.random_entry, name="random")
